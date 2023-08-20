63c25b5a32f0e9155403cd6af8d98ef3741221d9
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-08-20 14:17:54 +0530

    api working  test done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+// import { useState, useEffect} from 'react'
+import {useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api.js'
 
 function App() {
-  
+
+  useEffect( () => {
+    apiTesting();
+  }, []);
+
+  const apiTesting  = () => {
+    fetchDataFromApi('/movie/popular').then( (res) => { console.log(res) })
+  }
+
+
   return (
-    <>
-      
-    </>
+    <div className='App' >App</div>
   )
 }
 
