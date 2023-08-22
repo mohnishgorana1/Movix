6535fb84659d693a99932241be0ddfb0219ea9a2
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-08-22 15:40:55 +0530

    tailwind init

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.scss'
+import './index.css'
 
 import { store } from './store/store.js'
 import { Provider } from 'react-redux'
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -3,7 +3,15 @@ import './style.scss'
 
 function HeroBanner() {
   return (
-    <div>HeroBanner</div>
+    <div className='heroBanner'>
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title"></span>
+          <span className="subTitle">Millions of movies, TV shows and people to discover. Explore now.</span>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
