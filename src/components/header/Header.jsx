10183894a1ed52d18160628e4228e26061c641fe
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-08-23 11:59:18 +0530

    header init

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
 
-import './style.scss'
+import ContentWrapper from '../contentWrapper/ContentWrapper.jsx'
+import logo from '../../assets/movix-logo.svg'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 function Header() {
+
+  const [ show, setShow ] = useState("top")
+  const [ lastScrollY, setLastScrollY ] = useState(0)
+  const [ mobileMenu, setMobileMenu ] = useState(false)
+  const [ query, setQuery ] = useState("")
+  const [ showSearch, setShowSearch ] = useState("")
+
+  const navigate = useNavigate();
+  const location = useLocation()
+
   return (
     <div>Header</div>
   )
